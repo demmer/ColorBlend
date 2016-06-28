952d354a0f832945978f5c443c29a92cd3cd9a69
Michael Demmer <demmer@gmail.com>
2016-06-28 13:58:30 -0700

    reduce font size

--- ColorBlend/Constants.swift
+++ ColorBlend/Constants.swift
@@ -11,7 +11,7 @@ import UIKit
 struct Constants {
     static let LabelFont = "Thonburi"
     static let LabelFontActive = "Thonburi-Bold"
-    static let LabelFontSize: CGFloat = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 12 : 24;
+    static let LabelFontSize: CGFloat = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 11 : 24;
     static let TitleFontSize: CGFloat = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 24 : 36;
 
 }
